22a288f24a9ca53aecc398c4b3bd10837b2b9733
Jeff <jeff.bertucci@gmail.com>
2016-09-27 00:06:53 -0600

    simple validator work

--- week2/simple-validator/main.js
+++ week2/simple-validator/main.js
@@ -1,7 +1,20 @@
-var phoneNumber = prompt("Do you wish to move forward?\nGimme a PHONE NUMBER!...\n...and it better be written in xxx-xxx-xxxx format son!");
+var phoneNumber = prompt("Do you wish to move forward?"
+  + "\nGimme a PHONE NUMBER!...\n...and it better be written in"
+  + "xxx-xxx-xxxx format son!");
 
-phoneNumber.includes("-");
+function validatePhoneNumber(properPhone) {
+  return properPhone.charAt(3) === "-" && properPhone.charAt(7) === "-";
+}
+alert(validatePhoneNumber(phoneNumber));
 
+var birthDate = prompt("When were you born????"
+  + "\nSeriously I need to know!\n...and it better be written in"
+  + "MM/DD/YY format you wascally wabbit!");
+
+function validateBirthDate(properDate) {
+  return properDate.charAt(2) === "/" && properDate.charAt(5) === "/";
+}
+alert(validateBirthDate(birthDate));
 
 
 
@@ -10,11 +23,11 @@ phoneNumber.includes("-");
 // phoneNumber.indexOf("-");
 // phoneNumber.lastIndexOf("-");
 // phoneNumber.split('').forEach
-// phoneNumber.forEach(function(prompt) { 
-//   if (!parseInt[3] && [7] !== '-') { 
+// phoneNumber.forEach(function(prompt) {
+//   if (!parseInt[3] && [7] !== '-') {
 //      return 'Not Valid!!!!!';
-//   } 
-// }); 
+//   }
+// });
 
 
 
@@ -25,4 +38,4 @@ phoneNumber.includes("-");
 //     alert = "Try Again";
 // }
 
-// alert("You entered: " + answer + "\nThere are " + answer.length + " characters in the word" + "\nThe third character is " + answer.charAt(2) + "\nLowercase: " + answer.toLowerCase() + "\nUppercase: " + answer.toUpperCase() + "\nHow much " + answer + " could a " + answer + "chuck chuck if a " + answer + "chuck could chuck " + answer + "!" + "\nSubword: " + answer.substr(1,3))   
+// alert("You entered: " + answer + "\nThere are " + answer.length + " characters in the word" + "\nThe third character is " + answer.charAt(2) + "\nLowercase: " + answer.toLowerCase() + "\nUppercase: " + answer.toUpperCase() + "\nHow much " + answer + " could a " + answer + "chuck chuck if a " + answer + "chuck could chuck " + answer + "!" + "\nSubword: " + answer.substr(1,3))
