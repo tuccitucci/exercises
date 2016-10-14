4b48e7be410dd55bbf92b9a3c6f1c1eadf4eea00
Jeff <jeff.bertucci@gmail.com>
2016-10-14 17:05:26 -0600

    J3T.js project

--- Projects/truffl/components/main/main.js
+++ Projects/truffl/components/main/main.js
@@ -19,16 +19,10 @@ function mainController() {
 
     function updateSuggestion() {
       var suggestions = [
-        "Eat More!",
-        "Bananas!",
-        "Oranges!",
-        "You're gonna die!",
-        "fdfas",
-        "fdfaadfdsafs",
-        "agfdgdfsgsdfg",
-        "agfdgdfsgsdfg dsfdfadf",
-        "agfdgdfsgsdfg dfgsfgdsfg",
-        "oieurtoajt"
+        "You need more calcium in your diet...Have some yogurt!",
+        "Your magnesium intake is unacceptable...organic brown rice would help!",
+        "Do you have a problem with Vitamin D? No? Good...eat some tuna!",
+        "Brussel Sprouts! No? Well figure out something else with Vitamin C to eat!",
       ];
 
       ctrl.currentSuggestion = suggestions[Math.floor(Math.random() * (suggestions.length-1)) + 1];
